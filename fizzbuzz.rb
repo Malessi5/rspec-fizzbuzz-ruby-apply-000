28ada6af4b19efc25e76f667e11fafaaa7f353c9
Mike Alessi <mikealessi5@gmail.com>
2020-10-16 15:16:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
   if int % 3 == 0
-    puts "Fizz"
+    return "Fizz"
 
   elsif int % 5 == 0
     puts "Buzz"
@@ -11,7 +11,7 @@ def fizzbuzz (int)
     puts "Fizzbuzz"
 
   else
-    put "nil"
+    puts "nil"
   end
 
 end
