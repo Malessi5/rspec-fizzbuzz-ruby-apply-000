557510604e6b52f116b12dc642118df9c98af467
Mike Alessi <mikealessi5@gmail.com>
2020-10-16 15:19:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
 
-elsif (int % 3 == 0 && int % 5 == 0)
+elsif (int % 3 == 0) && (int % 5 == 0)
   return "Fizzbuzz"
 
   if int % 3 == 0
